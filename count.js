817384e9199a97c381f8a4e61749dc17c154839b
mgmob@yandex.ru <Papazol123>
2020-06-22 14:50:34 +0300

    22-06-2020-14:50 - Готово всё

--- count.js
+++ count.js
@@ -1,3 +1,35 @@
+/***
+ number - исходное число
+ decimals - количество знаков после разделителя
+ dec_point - символ разделителя
+ thousands_sep - разделитель тысячных
+ ***/
+function number_format(number, decimals, dec_point, thousands_sep) {
+    number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+    var n = !isFinite(+number) ? 0 : +number,
+        prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+        sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+        dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+        s = '',
+        toFixedFix = function(n, prec) {
+            var k = Math.pow(10, prec);
+            return '' + (Math.round(n * k) / k)
+                .toFixed(prec);
+        };
+    // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n))
+        .split('.');
+    if (s[0].length > 3) {
+        s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+    }
+    if ((s[1] || '')
+        .length < prec) {
+        s[1] = s[1] || '';
+        s[1] += new Array(prec - s[1].length + 1)
+            .join('0');
+    }
+    return s.join(dec);
+}
 function listClck(){
     //alert("OK!");
     var sI1=document.Test2.Item.selectedIndex;
@@ -91,14 +123,37 @@ function btnClick2()
 
     var T=a[Number(sI)];
 
-    document.getElementById("norm").innerHTML="Норматив потребления "+String(T)+" кг/год на единицу";
+    document.getElementById("norm").innerHTML="Норматив потребления "+number_format(T, 2, ',', ' ')+" кг/год на единицу";
     var i0=document.Test2.v1.value;
     var i1=document.Test2.v2.value;
-    document.getElementById("result1").innerHTML="Тариф: "+String((T/12)*i0*i1*5.89511)+" за период";
+    document.getElementById("result1").innerHTML="Тариф: "+number_format((T/12)*i0*i1*5.89511,2,',',' ')+" за период";
     var txt1 = "";
     for (let i = 1; i <= i1; i++) {
-        txt1 = txt1 + "<p>Тариф за " + String(i) + " месяц: "+ String((T/12)*i0*5.89511)+"</p>"
+        txt1 = txt1 + "<p>Тариф за " + String(i) + " месяц: "+ number_format((T/12)*i0*5.89511, 2, ',', ' ')+"</p>"
     }
     document.getElementById("result2").innerHTML="<p>Тариф по месяцам</p>"+txt1
+
+}
+
+function btnClick3()
+{
+    var c = [33.1428571428571, 	66.2368421052632, 	27.7301587301587, 	75, 	65.8745614035088, 	73.0931174089069, 	55.8899082568807, 	156.75, 	35.6503298774741, 	72.0476190476191, 	94.4242424242424, 	117.818181818182, 	27.5, 	52.1392405063291, 	101.636363636364, 	14.2214285714286, 	124.814814814815, 	57.875, 	101, 	81.9285714285714, 	25.0588235294118, 	28.5968253968254, 	99.3627450980392, 	63.3333333333333, 	117.240875912409]
+    var sIc=document.Test2.Item.selectedIndex;
+
+    var T=c[Number(sIc)];
+
+    var k0=document.Test2.k1.value;
+    var k1=document.Test2.k2.value;
+    var k2=document.Test2.k3.value;
+
+    document.getElementById("normk").innerHTML="<p>Плотность мусора: "+number_format(T, 2, ',', ' ')+" кг/м<sup>3</sup></p>" +
+        "<p>Количество ТКО в месяц, т: "+number_format(T*k0*k1*k2/1000, 2, ',', ' ')+"</p>";
+
+    document.getElementById("resultk1").innerHTML="Тариф: "+number_format(T*k0*k1*k2*5.89511, 2, ',', ' ')+" за весь период";
+    var txtk = "";
+    for (let i = 1; i <= k2; i++) {
+        txtk = txtk + "<p>Тариф за " + String(i) + " месяц: "+ number_format(T*k0*k1*5.89511, 2, ',', ' ')+"</p>"
+    }
+    document.getElementById("resultk2").innerHTML="<p>Тариф по месяцам:</p>"+txtk
 
 }
